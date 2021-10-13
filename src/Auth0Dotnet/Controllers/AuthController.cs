93212f8aeec86c90d7952ca73b125a9141383752
Luiz Lelis <luizhlelis@gmail.com>
2021-10-13 19:18:07 -0300

    fix: auth controller and paths

---
+++ src/Auth0Dotnet/Controllers/AuthController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Auth0Dotnet.Controllers
+{
+    [ApiVersion("1")]
+    [Route("v{version:apiVersion}/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        [HttpGet("sign-in")]
+        public IActionResult SignIn()
+        {
+            return Ok("Boa! Conseguiu");
+        }
+
+        //// GET api/auth/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST api/auth
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        //// PUT api/auth/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        // DELETE api/auth/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
