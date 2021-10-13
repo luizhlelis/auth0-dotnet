d5ad98d66ba97f159cbcbf00824e2b020afa1aa3
Luiz Lelis <luizhlelis@gmail.com>
2021-10-13 11:07:33 -0300

    fix: update main `terraform` to make the `apply` avaiable

---
+++ src/Terraform/.terraform.lock.hcl
@@ -0,0 +1,22 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/alexkappa/auth0" {
+  version     = "0.21.0"
+  constraints = "0.21.0"
+  hashes = [
+    "h1:1hmejx06OnCdtO7TVb94iljlu5aGb4xyDnG9XGPGvRY=",
+    "zh:021bfe7e1c4ed9c4da17afd3b5a1196470d0854f954dc810be4440e44ece459f",
+    "zh:0f1a70e8389b82fc45fd09266fb269c64cc62db7d4d1235491fbea8e05530b18",
+    "zh:46bc4baca0ca2140e34428bd59e424f1032f2dc2e2c9c07d51585840dca80990",
+    "zh:597c8903027bc8465b700fa8da89edcb72d0897930ad5f2f6ec1c48587ae3e24",
+    "zh:62ca951a16da28ee735c15a40cdde346dfcaa0b503cb5029fb3251377946209f",
+    "zh:9754f67bc8e18e7916e396f260c30a8285835b23e1a0f52e873464e2b8d4e1e0",
+    "zh:a9e737b2160e806248756a4e9f209e2b25a0d1a90a586c7423b922280a075d8e",
+    "zh:c6b1ae32f96e78755328be280537a7e3a67f7c338082d560984dbb285d5439ba",
+    "zh:c8c01583336d669762add9547088ccb46647e7abd743f16eba29bc35532cce5c",
+    "zh:da39303f6bec5594813fe75327d8245529c939815cdb115ed0ef83a6c9707b02",
+    "zh:e1e244cb624e600565d5709b3b63158221b18744969cadbd3817ec961d97a9a4",
+    "zh:f22f43ebe15cc5fcd16fcc82625443396fe7031d4cf6ba2925360a9b7003c9c9",
+  ]
+}
--- src/Terraform/main.tf
+++ src/Terraform/main.tf
@@ -1,10 +1,24 @@
+terraform {
+  required_providers {
+    auth0 = {
+      source  = "alexkappa/auth0"
+      version = "0.21.0"
+    }
+  }
+}
+
 provider "auth0" {}
 
+# auth0_branding is not updating the logo
 resource "auth0_branding" "auth_server_branding" {
     logo_url = "https://raw.githubusercontent.com/luizhlelis/luizhlelis/master/lelis-octocat.png"
+    favicon_url = "https://raw.githubusercontent.com/luizhlelis/luizhlelis/master/lelis-octocat.png"
     colors {
         primary = "#BB86FC"
         page_background = "#121212"
+    }
+    font {
+      url = "https://fonts.googleapis.com/css2?family=Nunito&display=swap"
     }
 }
 
@@ -16,7 +30,7 @@ resource "auth0_client" "auth0_dotnet_client" {
   is_first_party = true
   is_token_endpoint_ip_header_trusted = true
   token_endpoint_auth_method = "client_secret_post"
-  oidc_conformant = false
+  oidc_conformant = true
   callbacks = [ "https://localhost:5001/response-oidc" ]
   allowed_origins = [ "https://localhost" ]
   grant_types = [ "authorization_code", "refresh_token" ]
@@ -34,35 +48,12 @@ resource "auth0_client" "auth0_dotnet_client" {
     rotation_type = "rotating"
     expiration_type = "expiring"
     leeway = 15
-    token_lifetime = 84600
+    token_lifetime = 1296000
     infinite_idle_token_lifetime = true
     infinite_token_lifetime      = false
-    idle_token_lifetime          = 1296000
+    idle_token_lifetime          = 84600
   }
   client_metadata = {
     foo = "zoo"
-  }
-  addons {
-    firebase = {
-      client_email = "john.doe@example.com"
-      lifetime_in_seconds = 1
-      private_key = "wer"
-      private_key_id = "qwreerwerwe"
-    }
-    samlp {
-      audience = "https://example.com/saml"
-      mappings = {
-        email = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress"
-        name = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name"
-      }
-      create_upn_claim = false
-      passthrough_claims_with_no_mapping = false
-      map_unknown_claims_as_is = false
-      map_identities = false
-      name_identifier_format = "urn:oasis:names:tc:SAML:2.0:nameid-format:persistent"
-      name_identifier_probes = [
-        "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress"
-      ]
-    }
   }
 }
