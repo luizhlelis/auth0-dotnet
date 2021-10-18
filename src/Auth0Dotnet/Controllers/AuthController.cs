616b70cb404fe3e1281d35ff13f7e57941ff9de8
Luiz Lelis <luizhlelis@gmail.com>
2021-10-18 16:47:53 -0300

    chore: api update to `login`, `logout` and `me`

--- src/Auth0Dotnet/Controllers/AuthController.cs
+++ src/Auth0Dotnet/Controllers/AuthController.cs
@@ -1,7 +1,7 @@
-﻿using System;
-using System.Globalization;
+﻿using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -25,72 +25,41 @@ namespace Auth0Dotnet.Controllers
             _logger = logger;
         }
 
-        [HttpGet("sign-in")]
-        public async Task SignIn()
+        [HttpGet("login")]
+        public async Task Login()
         {
             await HttpContext
                 .ChallengeAsync(
                     "Auth0",
                     new AuthenticationProperties() { RedirectUri = _configuration["AuthorizationServer:RedirectUri"] }
                 );
-
-            _logger.LogInformation("SignIn was called");
         }
 
-        //// GET api/auth/5
-        //[HttpGet("{id}")]
-        //public string Get(int id)
-        //{
-        //    return "value";
-        //}
-
-        //// POST api/auth
-        //[HttpPost]
-        //public void Post([FromBody] string value)
-        //{
-        //}
-
-        //// PUT api/auth/5
-        //[HttpPut("{id}")]
-        //public void Put(int id, [FromBody] string value)
-        //{
-        //}
-
+        [Authorize]
         [HttpGet("me")]
-        public async Task<IActionResult> Me()
+        public IActionResult Me()
         {
-            // If the user is authenticated, then this is how you can get the access_token and id_token
-            if (User.Identity.IsAuthenticated)
+            return Ok(
+                new {
+                    Name = User.Identity.Name,
+                    EmailAddress = User.FindFirst(c => c.Type == ClaimTypes.Email)?.Value,
+                    ProfileImage = User.FindFirst(c => c.Type == "picture")?.Value
+                }
+            );
+        }
+
+        [Authorize]
+        [HttpGet("logout")]
+        public async Task Logout()
+        {
+            await HttpContext.SignOutAsync("Auth0", new AuthenticationProperties
             {
-                string accessToken = await HttpContext.GetTokenAsync("access_token");
-
-                // if you need to check the access token expiration time, use this value
-                // provided on the authorization response and stored.
-                // do not attempt to inspect/decode the access token
-                DateTime accessTokenExpiresAt = DateTime.Parse(
-                    await HttpContext.GetTokenAsync("expires_at"),
-                    CultureInfo.InvariantCulture,
-                    DateTimeStyles.RoundtripKind);
-
-                string idToken = await HttpContext.GetTokenAsync("id_token");
-
-                // Now you can use them. For more info on when and how to use the
-                // access_token and id_token, see https://auth0.com/docs/tokens
-            }
-
-            return Ok("Welcome Bro!");
+                // Indicate here where Auth0 should redirect the user after a logout.
+                // Note that the resulting absolute Uri must be whitelisted in the 
+                // **Allowed Logout URLs** settings for the client.
+                RedirectUri = Url.Action("Index", "Home")
+            });
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
         }
-
-        [HttpGet("sign-out")]
-        public void SignOutOidc()
-        {
-        }
-
-        //[Authorize]
-        //[HttpGet("response-oidc")]
-        //public void ResponseOidc()
-        //{
-        //    _logger.LogInformation("Callback was called");
-        //}
     }
 }
