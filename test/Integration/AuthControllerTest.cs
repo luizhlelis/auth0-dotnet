49074f6d68a066b12299e31562e62fe2b55d80f4
Luiz Lelis <luizhlelis@gmail.com>
2021-12-01 19:48:29 -0300

    chore: first test to auth controller

---
+++ test/Integration/AuthControllerTest.cs
@@ -0,0 +1,38 @@
+﻿using Xunit;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net.Http;
+using System.Threading.Tasks;
+using FluentAssertions;
+using FluentAssertions.Web;
+using Microsoft.Extensions.Configuration;
+
+namespace Auth0Dotnet.Test.Integration
+{
+    public class AuthControllerTest
+    {
+        private readonly HttpClient _client;
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public AuthControllerTest()
+        {
+            // constructs the testing server with the HostBuilder configuration
+            _factory = new WebApplicationFactory<Startup>();
+            _client = _factory.CreateClient();
+        }
+
+        [Fact(DisplayName = "Should return found with redirect url when login")]
+        public async Task ShouldReturnFoundWithRedirectUrlWhenLoginAsync()
+        {
+            // Act
+            var response = await _client.GetAsync("v1/Auth/login");
+
+            // Assert
+            var responseRequestUri = response.RequestMessage.RequestUri;
+
+            responseRequestUri.AbsolutePath
+                .Should().BeEquivalentTo("/authorize");
+            responseRequestUri.Authority
+                .Should().BeEquivalentTo("auth.luizlelis.com");
+        }
+    }
+}
